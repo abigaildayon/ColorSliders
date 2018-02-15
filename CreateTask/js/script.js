ed21955ab9a76584afc2332622245066d27c9677
abigaildayon <32396704+abigaildayon@users.noreply.github.com>
2018-02-14 22:26:10 -0500

    Initial commit

---
+++ CreateTask/js/script.js
@@ -0,0 +1,368 @@
+//Table 1
+$(document).ready(function () {
+//Box11	
+	$('.box11').mouseleave(function(event) {
+		$('.box11').stop().animate({
+			opacity : '1',
+			width : "100px",
+		}, 500, function() {		//animation complete
+			$('.box11').addClass('hovered');
+		});
+    });
+	$('.box11').mouseover(function(event) {
+		$('.box11').stop().animate({
+			opacity : '.75',
+			width : "300px",
+		}, 200, function() {		//animation complete
+			$('.box11').removeClass('hovered');
+		});
+    });
+    
+//Box12
+   	$('.box12').mouseleave(function(event) {
+		$('.box12').stop().animate({
+			opacity : '1',
+			width : "100px",
+		}, 500, function() {		//animation complete
+			$('.box12').addClass('hovered');
+		});
+    });
+	$('.box12').mouseover(function(event) {
+		$('.box12').stop().animate({
+			opacity : '.75',
+			width : "300px",
+		}, 200, function() {		//animation complete
+			$('.box12').removeClass('hovered');
+		});
+    });
+    
+//Box13
+	$('.box13').mouseleave(function(event) {
+		$('.box13').stop().animate({
+			opacity : '1',
+			width : "100px",
+		}, 500, function() {		//animation complete
+			$('.box13').addClass('hovered');
+		});
+    });
+	$('.box13').mouseover(function(event) {
+		$('.box13').stop().animate({
+			opacity : '.75',
+			width : "300px",
+		}, 200, function() {		//animation complete
+			$('.box13').removeClass('hovered');
+		});
+    });
+    
+//Box14	
+	$('.box14').mouseleave(function(event) {
+		$('.box14').stop().animate({
+			opacity : '1',
+			width : "100px",
+		}, 500, function() {		//animation complete
+			$('.box14').addClass('hovered');
+		});
+    });
+	$('.box14').mouseover(function(event) {
+		$('.box14').stop().animate({
+			opacity : '.75',
+			width : "300px",
+		}, 200, function() {		//animation complete
+			$('.box14').removeClass('hovered');
+		});
+    });
+    
+//Box15
+    $('.box15').mouseleave(function(event) {
+		$('.box15').stop().animate({
+			opacity : '1',
+			width : "100px",
+		}, 500, function() {		//animation complete
+			$('.box15').addClass('hovered');
+		});
+    });
+	$('.box15').mouseover(function(event) {
+		$('.box15').stop().animate({
+			opacity : '.75',
+			width : "300px",
+		}, 200, function() {		//animation complete
+			$('.box15').removeClass('hovered');
+		});
+    });
+    
+//Box16
+	$('.box16').mouseleave(function(event) {
+		$('.box16').stop().animate({
+			opacity : '1',
+			width : "100px",
+		}, 500, function() {		//animation complete
+			$('.box16').addClass('hovered');
+		});
+    });
+	$('.box16').mouseover(function(event) {
+		$('.box16').stop().animate({
+			opacity : '.75',
+			width : "300px",
+		}, 200, function() {		//animation complete
+			$('.box16').removeClass('hovered');
+		});
+    });
+    
+//Box21	
+	$('.box21').mouseleave(function(event) {
+		$('.box21').stop().animate({
+			opacity : '1',
+			width : "100px",
+		}, 500, function() {		//animation complete
+			$('.box21').addClass('hovered');
+		});
+    });
+	$('.box21').mouseover(function(event) {
+		$('.box21').stop().animate({
+			opacity : '.75',
+			width : "300px",
+		}, 200, function() {		//animation complete
+			$('.box21').removeClass('hovered');
+		});
+    });
+    
+//Box22
+    $('.box22').mouseleave(function(event) {
+		$('.box22').stop().animate({
+			opacity : '1',
+			width : "100px",
+		}, 500, function() {		//animation complete
+			$('.box22').addClass('hovered');
+		});
+    });
+	$('.box22').mouseover(function(event) {
+		$('.box22').stop().animate({
+			opacity : '.75',
+			width : "300px",
+		}, 200, function() {		//animation complete
+			$('.box22').removeClass('hovered');
+		});
+    });
+    
+//Box23
+	$('.box23').mouseleave(function(event) {
+		$('.box23').stop().animate({
+			opacity : '1',
+			width : "100px",
+		}, 500, function() {		//animation complete
+			$('.box23').addClass('hovered');
+		});
+    });
+	$('.box23').mouseover(function(event) {
+		$('.box23').stop().animate({
+			opacity : '.75',
+			width : "300px",
+		}, 200, function() {		//animation complete
+			$('.box23').removeClass('hovered');
+		});
+    });
+    
+//Box24	
+	$('.box24').mouseleave(function(event) {
+		$('.box24').stop().animate({
+			opacity : '1',
+			width : "100px",
+		}, 500, function() {		//animation complete
+			$('.box24').addClass('hovered');
+		});
+    });
+	$('.box24').mouseover(function(event) {
+		$('.box24').stop().animate({
+			opacity : '.75',
+			width : "300px",
+		}, 200, function() {		//animation complete
+			$('.box24').removeClass('hovered');
+		});
+    });
+    
+//Box25
+    $('.box25').mouseleave(function(event) {
+		$('.box25').stop().animate({
+			opacity : '1',
+			width : "100px",
+		}, 500, function() {		//animation complete
+			$('.box25').addClass('hovered');
+		});
+    });
+	$('.box25').mouseover(function(event) {
+		$('.box25').stop().animate({
+			opacity : '.75',
+			width : "300px",
+		}, 200, function() {		//animation complete
+			$('.box25').removeClass('hovered');
+		});
+    });
+    
+//Box26
+	$('.box26').mouseleave(function(event) {
+		$('.box26').stop().animate({
+			opacity : '1',
+			width : "100px",
+		}, 500, function() {		//animation complete
+			$('.box26').addClass('hovered');
+		});
+    });
+	$('.box26').mouseover(function(event) {
+		$('.box26').stop().animate({
+			opacity : '.75',
+			width : "300px",
+		}, 200, function() {		//animation complete
+			$('.box26').removeClass('hovered');
+		});
+    });
+    
+//Box31	
+	$('.box31').mouseleave(function(event) {
+		$('.box31').stop().animate({
+			opacity : '1',
+			width : "100px",
+		}, 500, function() {		//animation complete
+			$('.box31').addClass('hovered');
+		});
+    });
+	$('.box31').mouseover(function(event) {
+		$('.box31').stop().animate({
+			opacity : '.75',
+			width : "300px",
+		}, 200, function() {		//animation complete
+			$('.box31').removeClass('hovered');
+		});
+    });
+    
+//Box32
+   	$('.box32').mouseleave(function(event) {
+		$('.box32').stop().animate({
+			opacity : '1',
+			width : "100px",
+		}, 500, function() {		//animation complete
+			$('.box32').addClass('hovered');
+		});
+    });
+	$('.box32').mouseover(function(event) {
+		$('.box32').stop().animate({
+			opacity : '.75',
+			width : "300px",
+		}, 200, function() {		//animation complete
+			$('.box32').removeClass('hovered');
+		});
+    });
+    
+//Box23
+	$('.box33').mouseleave(function(event) {
+		$('.box33').stop().animate({
+			opacity : '1',
+			width : "100px",
+		}, 500, function() {		//animation complete
+			$('.box33').addClass('hovered');
+		});
+    });
+	$('.box33').mouseover(function(event) {
+		$('.box33').stop().animate({
+			opacity : '.75',
+			width : "300px",
+		}, 200, function() {		//animation complete
+			$('.box33').removeClass('hovered');
+		});
+    });
+    
+//Box34	
+	$('.box34').mouseleave(function(event) {
+		$('.box34').stop().animate({
+			opacity : '1',
+			width : "100px",
+		}, 500, function() {		//animation complete
+			$('.box34').addClass('hovered');
+		});
+    });
+	$('.box34').mouseover(function(event) {
+		$('.box34').stop().animate({
+			opacity : '.75',
+			width : "300px",
+		}, 200, function() {		//animation complete
+			$('.box34').removeClass('hovered');
+		});
+    });
+    
+//Box35
+    	$('.box35').mouseleave(function(event) {
+		$('.box35').stop().animate({
+			opacity : '1',
+			width : "100px",
+		}, 500, function() {		//animation complete
+			$('.box35').addClass('hovered');
+		});
+    });
+	$('.box35').mouseover(function(event) {
+		$('.box35').stop().animate({
+			opacity : '.75',
+			width : "300px",
+		}, 200, function() {		//animation complete
+			$('.box35').removeClass('hovered');
+		});
+    });
+    
+//Box36
+		$('.box36').mouseleave(function(event) {
+		$('.box36').stop().animate({
+			opacity : '1',
+			width : "100px",
+		}, 500, function() {		//animation complete
+			$('.box36').addClass('hovered');
+		});
+    });
+	$('.box36').mouseover(function(event) {
+		$('.box36').stop().animate({
+			opacity : '.75',
+			width : "300px",
+		}, 200, function() {		//animation complete
+			$('.box36').removeClass('hovered');
+		});
+    });
+	
+	
+	
+      
+    function refreshSwatch() {
+    var color1 = $("#rSlider1").toString() + $("#gSlider1").toString() + $("#bSlider1").toString();
+      var red = $( "#rSlider1" ).slider( "value" ),
+        green = $( "#gSlider1" ).slider( "value" ),
+        blue = $( "#bSlider1" ).slider( "value" ),
+        hex = color1( red, green, blue );
+      $( "#swatch" ).css( "background-color", "#" + hex );
+    }
+      
+      
+/*	$("[type=range]").change(function(){
+		$("#rSlider1").change(function(){
+			alert("Hello");
+		var color1 = $("#rSlider1").toString() + $("#gSlider1").toString() + $("#bSlider1").toString();
+    	$("#sbox1").css("background-color", color1);
+    	
+	});
+*/
+	
+
+
+
+/*    
+//Color Sliders
+	var input = document.querySelectorAll("input");
+	
+	for(var i = 0; i < input.length; i++){
+		input(i).addEventListener("input", function(){
+			var red1 = document.getElementById("rSlider1").value,
+				green1 = document.getElementById("gSlider1").value,
+				blue1 = document.getElementById("bSlider1").value;
+				
+			var sbox1 = document.getElementById("sbox1");
+				sbox1.style.background = "rgb(" + red1+ ", " + green1+", " + blue1+")";
+		});
+	}
+  */  
+});
+;
